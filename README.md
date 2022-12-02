0eefa719f6bd4e03b4dad007b2d1c66e0bd3befb
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-02 12:12:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chap23-l4-4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/exemple-slider)
---
+++ src/app/slider-example.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "slider-example",
+  templateUrl: "slider-example.html"
+})
+export class SliderExample {}
